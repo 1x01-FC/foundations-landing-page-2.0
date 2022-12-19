dec29cb19c984da4af68ddfc5932afdaf071bd8f
Frederic C <toonvesper@gmail.com>
2022-12-19 16:41:02 -0500

    complete footer

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+function date() {
+    document.getElementById('year').innerHTML = new Date().getFullYear();
+}
